003137938fd749c0373a0392200a4f4dc39c756b
Debie <debiegarcia@hotmail.com>
2022-05-04 12:35:35 -0500

    Create Main Activity for Patient and Psychologist

---
+++ app/src/main/java/com/codetech/psychohelp/PatientActivity.kt
@@ -0,0 +1,22 @@
+package com.codetech.psychohelp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class PatientActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
